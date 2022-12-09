dea4d15968c5bb4fd55bb35cc4b314b7aedf7147
mtstall <mtstallings95@gmail.com>
2022-12-09 11:47:46 -0500

    code to return password, numeric only passwords generated, special character only passwords generated

--- Develop/script.js
+++ Develop/script.js
@@ -1,27 +1,16 @@
 // Assignment code here
 
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-//my code
-
-//array of possible password values
+//array of possible alpha values
 var passwordValues = "abcdefghijklmnopqrstuvwxyz";
 var passwordValuesArray = passwordValues.split('');
-console.log(passwordValuesArray);
+
+//array of possible numeric values
+var passwordNumericValues = "1234567890";
+var passwordNumericValuesArray = passwordNumericValues.split('');
+
+//array of possible special characters
+var passwordSpecialValues = "!\"#$%&'()*+,-./:;<=>?@[\}^_`{|}~";
+var passwordSpecialValuesArray = passwordSpecialValues.split('');
 
 //generatePassword function
 function generatePassword() {
@@ -31,12 +20,15 @@ function generatePassword() {
   
   //if statements for length validation
   console.log(passwordLength);
-  while ((passwordLength < 8) || (passwordLength > 128)) {
+  while ((passwordLength < 8) || (passwordLength > 128) || isNaN(passwordLength)) {
     if(passwordLength < 8) {
     window.alert("The password length you entered is too short. Enter a length greater than 8 and less than 128.")
     }
-    if(passwordLength > 128) {
+    else if(passwordLength > 128) {
     window.alert("The password length you entered is too long. Enter a length greater than 8 and less than 128.")
+    }
+    else if(isNaN(passwordLength)){
+      window.alert("Please enter a number.");
     }
     passwordLength = window.prompt("Enter desired password length (must be between 8 and 128 characters):");
   }
@@ -67,7 +59,7 @@ function generatePassword() {
     }
 
   //code for upperCase only selected
-  if((lowerCase == false) && (upperCase === true) && (numeric == false) && (special == false)) {
+  else if((lowerCase == false) && (upperCase === true) && (numeric == false) && (special == false)) {
     var passwordArray = [];
 
     for (var i = 0; i < passwordLength; i++) {
@@ -79,7 +71,45 @@ function generatePassword() {
     console.log(password);
   }
 
+    //code for numeric only selected
+    else if((lowerCase == false) && (upperCase === false) && (numeric == true) && (special == false)) {
+      var passwordArray = [];
+  
+      for (var i = 0; i < passwordLength; i++) {
+        var randomValue = passwordNumericValuesArray[Math.floor(Math.random() * passwordNumericValuesArray.length)];
+        passwordArray[i] = randomValue;
+      }
+      var password = passwordArray.join("");
+      password = password.toUpperCase();
+      console.log(password);
+    }
 
+      //code for special characters only selected
+      else if((lowerCase == false) && (upperCase === false) && (numeric == false) && (special == true)) {
+        var passwordArray = [];
+      
+        for (var i = 0; i < passwordLength; i++) {
+          var randomValue = passwordSpecialValuesArray[Math.floor(Math.random() * passwordSpecialValuesArray.length)];
+          passwordArray[i] = randomValue;
+        }
+        var password = passwordArray.join("");
+        password = password.toUpperCase();
+        console.log(password);
+      }
+      return password;
   }
 
-generatePassword();
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
